5b2750cd8bb9137461e0bff433f36361623f1ad1
Danyi Zhang <dz2494@columbia.edu>
2023-08-02 01:31:35 +0200

    Compute dimuon inv mass

--- CosmicsAnalyzer/MyAnalyzer/plugins/MyAnalyzer.cc
+++ CosmicsAnalyzer/MyAnalyzer/plugins/MyAnalyzer.cc
@@ -47,12 +47,15 @@
 
 #include "TFile.h"
 #include "TTree.h"
+#include "TLorentzVector.h"
 //
 // class declaration
 //
 
+const int kMuonNMax = 10000;
 const int kTrackNMax = 10000;
 const int kTrackRecHitsNMax = 100;
+const float kMuonMass = 0.10566; // GeV
 
 // If the analyzer does not use TFileService, please remove
 // the template argument to the base class so the class inherits
@@ -76,27 +79,42 @@ private:
   // ----------member data ---------------------------
   // edm::EDGetTokenT<TrackCollection> tracksToken_;  //used to select what tracks to read from configuration file
   edm::EDGetTokenT<std::vector<reco::Muon>> muonToken_;
-  edm::EDGetTokenT<std::vector<reco::Track>> tracksToken_;
-  edm::ESGetToken<DTGeometry, MuonGeometryRecord> muonDTGeomToken_;
+  // edm::EDGetTokenT<std::vector<reco::Track>> tracksToken_;
+  // edm::ESGetToken<DTGeometry, MuonGeometryRecord> muonDTGeomToken_;
   const DTGeometry* muonDTGeom;
 
-  std::vector<double> muonPhis_;  // vector of muon phi's to store in outfile
-  std::vector<double> muonPts_;
-  std::vector<double> muonEtas_;
+  int    muon_n_;
+  float  muon_pt_[kMuonNMax];
+  float  muon_p_[kMuonNMax];
+  float  muon_eta_[kMuonNMax];
+  float  muon_phi_[kMuonNMax];
+  float  muon_track_vx_[kMuonNMax];
+  float  muon_track_vy_[kMuonNMax];
+  float  muon_track_min_dv_;
+  float  muon_track_minDv_vx1_;  // positive phi
+  float  muon_track_minDv_vy1_;  // positive phi
+  float  muon_track_minDv_vz1_;  // positive phi
+  float  muon_track_minDv_vx2_;  // negative phi
+  float  muon_track_minDv_vy2_;  // negative phi
+  float  muon_track_minDv_vz2_;  // negative phi
+  int    diMuon_mass_;
 
+  /*
   int track_n_;
-  std::vector<double> track_vx_;
-  std::vector<double> track_vy_;
-  std::vector<double> track_vz_;
-  std::vector<double> track_phi_;
-  std::vector<double> track_py_;
-  float track_dtSeg_glbX_[kTrackNMax][kTrackRecHitsNMax];
-  float track_dtSeg_glbY_[kTrackNMax][kTrackRecHitsNMax];
-  float track_dtSeg_glbZ_[kTrackNMax][kTrackRecHitsNMax];
+  float  track_vx_[kTrackNMax];
+  float  track_vy_[kTrackNMax];
+  float  track_vz_[kTrackNMax];
+  float  track_phi_[kTrackNMax];
+  float  track_py_[kTrackNMax];
+  float  track_dtSeg_glbX_[kTrackNMax][kTrackRecHitsNMax];
+  float  track_dtSeg_glbY_[kTrackNMax][kTrackRecHitsNMax];
+  float  track_dtSeg_glbZ_[kTrackNMax][kTrackRecHitsNMax];
+  */
 
   // TFile* outputFile_;  // outfile 
   TTree* outputTree_;  // outtree
-  TTree* outputTree2_;  // outtree2
+  // TTree* outputTree2_;  // outtree2
+  
 };
 
 //
@@ -111,40 +129,16 @@ private:
 // constructors and destructor
 //
 MyAnalyzer::MyAnalyzer(const edm::ParameterSet& iConfig)
-    : muonToken_(consumes<std::vector<reco::Muon>>(iConfig.getParameter<edm::InputTag>("muonCollection"))),
-    tracksToken_(consumes<std::vector<reco::Track>>(iConfig.getParameter<edm::InputTag>("cosmicMuonCollection"))),
-    muonDTGeomToken_(esConsumes()) {
+    : muonToken_(consumes<std::vector<reco::Muon>>(iConfig.getParameter<edm::InputTag>("muonCollection")))
+    // tracksToken_(consumes<std::vector<reco::Track>>(iConfig.getParameter<edm::InputTag>("cosmicMuonCollection"))),
+    // muonDTGeomToken_(esConsumes()) 
+    {
 
-  //now do what ever initialization is needed
-
-  // Get the TFileService
-  edm::Service<TFileService> fs;
-  // Create a TTree using the TFileService
-  outputTree_ = fs->make<TTree>("MuonTree", "Muon Distributions");
-  outputTree2_ = fs->make<TTree>("CosmicMuonTree", "Cosmic Muon Distributions");
-  // Add branches to the TTree
-  outputTree_->Branch("muonPhi", &muonPhis_);
-  outputTree_->Branch("muonPt", &muonPts_);
-  outputTree_->Branch("muonEta", &muonEtas_);
-
-  outputTree2_->Branch("track_vx", &track_vx_);
-  outputTree2_->Branch("track_vy", &track_vy_);
-  outputTree2_->Branch("track_vz", &track_vz_);
-  outputTree2_->Branch("track_phi", &track_phi_);
-  outputTree2_->Branch("track_py", &track_py_);
-  outputTree2_ -> Branch ( "track_n",    &track_n_);
-  outputTree2_ -> Branch ( "track_dtSeg_glbX", track_dtSeg_glbX_, "track_dtSeg_glbX[track_n][100]/F");
-  outputTree2_ -> Branch ( "track_dtSeg_glbY", track_dtSeg_glbY_, "track_dtSeg_glbY[track_n][100]/F");
-  outputTree2_ -> Branch ( "track_dtSeg_glbZ", track_dtSeg_glbZ_, "track_dtSeg_glbZ[track_n][100]/F");
 }
 
 MyAnalyzer::~MyAnalyzer() {
   // do anything here that needs to be done at desctruction time
   // (e.g. close files, deallocate resources etc.)
-  // 
-  // please remove this method altogether if it would be left empty
-
-  // delete outputTree_;
 
 }
 
@@ -154,42 +148,92 @@ MyAnalyzer::~MyAnalyzer() {
 
 // ------------ method called for each event  ------------
 void MyAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
-  // using namespace edm;
-  
+  using namespace edm;
   using namespace std;
 
   // Handle: a smart pointer and is used to hold and access data from the event
   edm::Handle<std::vector<reco::Muon>> muons;
   iEvent.getByToken(muonToken_, muons);  // retrieve data products from the event using tokens
-  muonDTGeom = &iSetup.getData(muonDTGeomToken_);
+  // muonDTGeom = &iSetup.getData(muonDTGeomToken_);
 
+  muon_track_min_dv_ = 999.;
+  muon_track_minDv_vx1_ = 999.;
+  muon_track_minDv_vy1_ = 999.;
+  muon_track_minDv_vz1_ = 999.;
+  muon_track_minDv_vx2_ = 999.;
+  muon_track_minDv_vy2_ = 999.;
+  muon_track_minDv_vz2_ = 999.;
+  diMuon_mass_ = 999.;
+  TLorentzVector v1;
+  TLorentzVector v2;
+
+  muon_n_ = 0;
   for (const auto& muon : *muons) {
-    double phi = muon.phi();
-    double pt = muon.pt();
-    double eta = muon.eta();
-    muonPhis_.push_back(phi);
-    muonPts_.push_back(pt);
-    muonEtas_.push_back(eta);
-    // Fill the TTree with the data for each muon
-    outputTree_->Fill();
+    muon_pt_[muon_n_] = muon.pt();
+    muon_p_[muon_n_] = muon.p();
+    muon_pt_[muon_n_] = muon.pt();
+    muon_eta_[muon_n_] = muon.eta();
+    muon_phi_[muon_n_] = muon.phi();
+    muon_n_ ++;
+
+    if (muon.outerTrack().isNonnull()) {
+      reco::TrackRef recoTrack = muon.outerTrack();
+      muon_track_vx_[muon_n_] = recoTrack->vx();
+      muon_track_vy_[muon_n_] = recoTrack->vy();
+
+      v1.SetPtEtaPhiM(muon.pt(), muon.eta(), muon.phi(), kMuonMass);
+      // at least two tracks
+      if (muons->size() >= 2) {
+        for (const auto& muon2 : *muons) {
+          // choose two tracks in opposite direction
+          if (muon.phi() * muon2.phi() < 0 && muon2.outerTrack().isNonnull()) {
+            reco::TrackRef recoTrack2 = muon2.outerTrack();
+            double dx = recoTrack->vx() - recoTrack2->vx();
+            double dy = recoTrack->vy() - recoTrack2->vy();
+            double dz = recoTrack->vz() - recoTrack2->vz();
+            double dxy = sqrt(pow(dx, 2) + pow(dy, 2) + pow(dz, 2));
+            // cout << "muon vx = " << recoTrack->vx() << ", vy = " << recoTrack->vy();
+            // cout << "muon2 vx = " << recoTrack2->vx() << ", vy = " << recoTrack2->vy();
+            // cout << "dxy is "<< dxy << endl; 
+
+            if (dxy < muon_track_min_dv_) {
+              muon_track_min_dv_ = dxy;
+              muon_track_minDv_vx1_ = (muon.phi() > 0) ? muon.vx() : muon2.vx();
+              muon_track_minDv_vy1_ = (muon.phi() > 0) ? muon.vy() : muon2.vy();
+              muon_track_minDv_vz1_ = (muon.phi() > 0) ? muon.vz() : muon2.vz();
+              muon_track_minDv_vx2_ = (muon.phi() < 0) ? muon.vx() : muon2.vx();
+              muon_track_minDv_vy2_ = (muon.phi() < 0) ? muon.vy() : muon2.vy();
+              muon_track_minDv_vz2_ = (muon.phi() < 0) ? muon.vz() : muon2.vz();
+
+              v2.SetPtEtaPhiM(muon2.pt(), muon2.eta(), muon2.phi(), kMuonMass);
+              diMuon_mass_ = (v1 + v2).M();
+            }
+          }
+        }
+      }
+    }
   }
 
+  // Fill the TTree with the data for each muon
+  outputTree_->Fill();
+
+  /*
   edm::Handle<std::vector<reco::Track>> tracks;
   iEvent.getByToken(tracksToken_, tracks);
   track_n_ = 0;
   for (const auto& track : *tracks) {
-    track_vx_.push_back(track.vx());
-    track_vy_.push_back(track.vy());
-    track_vz_.push_back(track.vz());
-    track_phi_.push_back(track.phi());
-    track_py_.push_back(track.py());
+    track_vx_[track_n_] = track.vx();
+    track_vy_[track_n_] = track.vy();
+    track_vz_[track_n_] = track.vz();
+    track_phi_[track_n_] = track.phi();
+    track_py_[track_n_] = track.py();
     
     int track_recHits_n_ = 0;
     for (trackingRecHit_iterator hit = track.recHitsBegin(); hit != track.recHitsEnd(); ++hit) {
       const TrackingRecHit* recHit = *hit;
       if (recHit->isValid()) {
-        // GlobalPoint globalPos = recHit->globalPosition();
         DetId hitId = recHit->geographicalId();
+        // pick recHits from the DT segment
         if (hitId.det() == DetId::Muon && hitId.subdetId() == MuonSubdetId::DT){
           const GeomDet* dtDet = muonDTGeom->idToDet(hitId);
           GlobalPoint globalPoint = dtDet->toGlobal(recHit->localPosition());
@@ -201,28 +245,58 @@ void MyAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
       }
     }
 
-    track_n_ ++;
-    outputTree2_->Fill();
+  track_n_ ++;
+    
   }
-  
 
-  // for (const auto& track : iEvent.get(tracksToken_)) {
-  //   // do something with track parameters, e.g, plot the charge.
-  //   // int charge = track.charge();
-  // }
+  outputTree2_->Fill();
+  */
 }
 
 // ------------ method called once each job just before starting event loop  ------------
 void MyAnalyzer::beginJob() {
-  // please remove this method if not needed
 
+  // Get the TFileService
+  edm::Service<TFileService> fs;
+  // Create a TTree using the TFileService
+  outputTree_ = fs->make<TTree>("MuonTree", "Muon Distributions");
+  // outputTree2_ = fs->make<TTree>("CosmicMuonTree", "Cosmic Muon Distributions");
+  // Add branches to the TTree
+
+  outputTree_  -> Branch("muon_n",   &muon_n_);
+  outputTree_  -> Branch("muon_pt",  muon_pt_, "muon_pt[muon_n]/F");
+  outputTree_  -> Branch("muon_p",   muon_p_,  "muon_p[muon_n]/F");
+  outputTree_  -> Branch("muon_eta", muon_eta_, "muon_eta[muon_n]/F");
+  outputTree_  -> Branch("muon_phi", muon_phi_, "muon_phi[muon_n]/F");
+  outputTree_  -> Branch("muon_track_vx", muon_track_vx_, "muon_track_vx[muon_n]/F");
+  outputTree_  -> Branch("muon_track_vy", muon_track_vy_, "muon_track_vy[muon_n]/F");
+  outputTree_  -> Branch("muon_track_min_dv",   &muon_track_min_dv_);
+  outputTree_  -> Branch("muon_track_minDv_vx1",   &muon_track_minDv_vx1_);
+  outputTree_  -> Branch("muon_track_minDv_vy1",   &muon_track_minDv_vy1_);
+  outputTree_  -> Branch("muon_track_minDv_vz1",   &muon_track_minDv_vz1_);
+  outputTree_  -> Branch("muon_track_minDv_vx2",   &muon_track_minDv_vx2_);
+  outputTree_  -> Branch("muon_track_minDv_vy2",   &muon_track_minDv_vy2_);
+  outputTree_  -> Branch("muon_track_minDv_vz2",   &muon_track_minDv_vz2_);
+  outputTree_  -> Branch("diMuon_mass",   &diMuon_mass_);
+
+  /*
+  outputTree2_ -> Branch( "track_n",          &track_n_);
+  outputTree2_ -> Branch( "track_vx",         track_vx_,         "track_vx[track_n]/F");
+  outputTree2_ -> Branch( "track_vy",         track_vy_,         "track_vy[track_n]/F");
+  outputTree2_ -> Branch( "track_vz",         track_vz_,         "track_vz[track_n]/F");
+  outputTree2_ -> Branch( "track_phi",        track_phi_,        "track_phi[track_n]/F");
+  outputTree2_ -> Branch( "track_py",         track_py_,         "track_py[track_n]/F");
+  outputTree2_ -> Branch( "track_dtSeg_glbX", track_dtSeg_glbX_, "track_dtSeg_glbX[track_n][100]/F");
+  outputTree2_ -> Branch( "track_dtSeg_glbY", track_dtSeg_glbY_, "track_dtSeg_glbY[track_n][100]/F");
+  outputTree2_ -> Branch( "track_dtSeg_glbZ", track_dtSeg_glbZ_, "track_dtSeg_glbZ[track_n][100]/F");
+  */
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
 void MyAnalyzer::endJob() {
   // please remove this method if not needed
-  outputTree_->Fill();
-  outputTree2_->Fill();
+  // outputTree_->Fill();
+  // outputTree2_->Fill();
 }
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
--- CosmicsAnalyzer/MyAnalyzer/python/my_analyzer_cfg.py
+++ CosmicsAnalyzer/MyAnalyzer/python/my_analyzer_cfg.py
@@ -8,6 +8,7 @@ from FWCore.ParameterSet.VarParsing import VarParsing
 # cf: https://twiki.cern.ch/twiki/bin/view/CMS/CommandLineOptions
 #     FWCore/ParameterSet/python/VarParsing.py
 options = VarParsing('analysis')
+# global tag: https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideFrontierConditions#Analysis_recipes
 options.register('GTAG', '106X_upgrade2018_realistic_v11BasedCandidateTmp_2022_08_09_01_32_34',
     VarParsing.multiplicity.singleton,
     VarParsing.varType.string,
@@ -19,10 +20,10 @@ options.parseArguments()
 process = cms.Process("MYANALYZER")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.StandardSequences.MagneticField_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.load("Configuration.StandardSequences.Reconstruction_cff")
+# process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+# process.load('Configuration.StandardSequences.MagneticField_cff')
+# process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+# process.load("Configuration.StandardSequences.Reconstruction_cff")
 process.load('Configuration.StandardSequences.Services_cff')
 
 
@@ -33,54 +34,66 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source("PoolSource",
                                 # replace 'myfile.root' with the source file you want to use
                                 fileNames = cms.untracked.vstring(
-            'file:/afs/cern.ch/user/d/dazhang/private/cms/cosmics/data/2ad63d9f-234b-4c68-8c18-49d485d42bc7.root'
+            # 'file:/afs/cern.ch/user/d/dazhang/private/cms/cosmics/data/2ad63d9f-234b-4c68-8c18-49d485d42bc7.root'
+            'file:/eos/user/d/dazhang/cms/ZMu/28e81690-aa38-4934-9ad6-05b2797148a1.root'
                 )
                             )
 
-from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, options.GTAG, '')
+# from Configuration.AlCa.GlobalTag import GlobalTag
+# process.GlobalTag = GlobalTag(process.GlobalTag, options.GTAG, '')
 
 # configure the analysis module named "MyAnalyzer"
+# process.muonAnalyzer = cms.EDAnalyzer("MyAnalyzer",
+#          muonCollection = cms.InputTag("muons"),
+#          cosmicMuonCollection = cms.InputTag("cosmicMuons")
+#                                         )
+
+# process.muon1LegAnalyzer = cms.EDAnalyzer("MyAnalyzer",
+#          muonCollection = cms.InputTag("muons1Leg"),
+#          cosmicMuonCollection = cms.InputTag("cosmicMuons1Leg")
+#                                         )
+
+# process.muonEndCapsOnlyAnalyzer = cms.EDAnalyzer("MyAnalyzer",
+#          muonCollection = cms.InputTag("muonsBeamHaloEndCapsOnly"),
+#          cosmicMuonCollection = cms.InputTag("cosmicMuonsEndCapsOnly")
+#                                         )
+
+# process.muonNoRPCAnalyzer = cms.EDAnalyzer("MyAnalyzer",
+#          muonCollection = cms.InputTag("muonsNoRPC"),
+#          cosmicMuonCollection = cms.InputTag("cosmicMuonsNoRPC")
+#                                         )
+
+# process.muonWitht0CorrectionAnalyzer = cms.EDAnalyzer("MyAnalyzer",
+#          muonCollection = cms.InputTag("muonsWitht0Correction"),
+#          cosmicMuonCollection = cms.InputTag("cosmicMuonsWitht0Correction")
+#                                         )
+
+# process.splitmuonAnalyzer = cms.EDAnalyzer("MyAnalyzer",
+#          muonCollection = cms.InputTag("splitMuons"),
+#          cosmicMuonCollection = cms.InputTag("cosmicMuons")
+#                                         )
+
+# process.lhcSTAmuonAnalyzer = cms.EDAnalyzer("MyAnalyzer",
+#          muonCollection = cms.InputTag("lhcSTAMuons"),
+#          cosmicMuonCollection = cms.InputTag("cosmicMuons")
+#                                         )
+
+# process.lhcSTAMuonAnalyzer = cms.EDAnalyzer("MyAnalyzer",
+#          muonCollection = cms.InputTag("lhcSTAMuons"),
+#          cosmicMuonCollection = cms.InputTag("cosmicMuons")
+#                                         )
+
 process.muonAnalyzer = cms.EDAnalyzer("MyAnalyzer",
-         muonCollection = cms.InputTag("muons"),
-         cosmicMuonCollection = cms.InputTag("cosmicMuons")
-                                        )
-
-process.muon1LegAnalyzer = cms.EDAnalyzer("MyAnalyzer",
-         muonCollection = cms.InputTag("muons1Leg"),
-         cosmicMuonCollection = cms.InputTag("cosmicMuons1Leg")
-                                        )
-
-process.muonEndCapsOnlyAnalyzer = cms.EDAnalyzer("MyAnalyzer",
-         muonCollection = cms.InputTag("muonsBeamHaloEndCapsOnly"),
-         cosmicMuonCollection = cms.InputTag("cosmicMuonsEndCapsOnly")
-                                        )
-
-process.muonNoRPCAnalyzer = cms.EDAnalyzer("MyAnalyzer",
-         muonCollection = cms.InputTag("muonsNoRPC"),
-         cosmicMuonCollection = cms.InputTag("cosmicMuonsNoRPC")
-                                        )
-
-process.muonWitht0CorrectionAnalyzer = cms.EDAnalyzer("MyAnalyzer",
-         muonCollection = cms.InputTag("muonsWitht0Correction"),
-         cosmicMuonCollection = cms.InputTag("cosmicMuonsWitht0Correction")
-                                        )
-
-process.splitmuonAnalyzer = cms.EDAnalyzer("MyAnalyzer",
-         muonCollection = cms.InputTag("splitMuons"),
-         cosmicMuonCollection = cms.InputTag("cosmicMuons")
-                                        )
-
-process.lhcSTAmuonAnalyzer = cms.EDAnalyzer("MyAnalyzer",
-         muonCollection = cms.InputTag("lhcSTAMuons"),
-         cosmicMuonCollection = cms.InputTag("cosmicMuons")
+         muonCollection = cms.InputTag("muons")
+        #  cosmicMuonCollection = cms.InputTag("standAloneMuons")
                                         )
 
 
 # configure the output file for the analysis results
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string(
-        "/afs/cern.ch/user/d/dazhang/private/cms/CMSSW_13_0_10/src/CosmicsAnalyzer/MyAnalyzer/test/muon_ntuple.root"
+        # "/afs/cern.ch/user/d/dazhang/private/cms/CMSSW_13_0_10/src/CosmicsAnalyzer/MyAnalyzer/test/CosmicSP_lhcSTAMuon_ntuple.root"
+        "/afs/cern.ch/user/d/dazhang/private/cms/CMSSW_13_0_10/src/CosmicsAnalyzer/MyAnalyzer/test/ZMu_muons_ntuple.root"
         )
                                     )
 
@@ -92,3 +105,5 @@ process.p1 = cms.Path(process.muonAnalyzer)
 # process.p5 = cms.Path(process.muonWitht0CorrectionAnalyzer)
 # process.p6 = cms.Path(process.splitmuonAnalyzer)
 # process.p7 = cms.Path(process.lhcSTAmuonAnalyzer)
+
+# process.p = cms.Path(process.lhcSTAMuonAnalyzer)
